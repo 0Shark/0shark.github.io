8019f9f0b0aa7b6c19b3b613df0bce49a00dbda6
Juled Zaganjori <zaganjorijuled@gmail.com>
2022-06-19 12:08:45 +0200

    Restore projects

---
+++ projects/stafi-chatroom/container.php
@@ -0,0 +1,27 @@
+</head>
+<body class="" style="background-image: url('img/bg.png'); background-repeat: no-repeat; background-position: right top;">
+<div role="navigation" class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="http://www.stafi.gq" class="navbar-brand">STAFI.GQ</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="http://www.stafi.gq">Home</a></li>
+            <li><a href="http://detyra-shtepie.tk">Homework</a></li>
+           
+          </ul>
+         
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+	
+	<div class="container" style="min-height:500px;">
+	<div class=''>
+	</div>
